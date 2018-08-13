0d6992000c9c35b3f014a9da8fd759f5343fdb36
Luis Moral Guerrero <luis.moral@gmail.com>
2018-08-13 16:32:57 +0200

    Added required class.

---
+++ src/main/java/ScoreKeeper.java
@@ -0,0 +1,30 @@
+public class ScoreKeeper {
+
+    public void scoreTeamA1() {
+
+    }
+
+    public void scoreTeamA2() {
+
+    }
+
+    public void scoreTeamA3() {
+
+    }
+
+    public void scoreTeamB1() {
+
+    }
+
+    public void scoreTeamB2() {
+
+    }
+
+    public void scoreTeamB3() {
+
+    }
+
+    public String getScore() {
+        return null;
+    }
+}
