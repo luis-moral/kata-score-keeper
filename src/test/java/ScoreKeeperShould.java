7279285e93fcfeb9488f60c85c118fbfb30dcbf3
Luis Moral Guerrero <luis.moral@gmail.com>
2018-08-13 18:20:21 +0200

    Improved should_display_game_score test.

--- src/test/java/ScoreKeeperShould.java
+++ src/test/java/ScoreKeeperShould.java
@@ -6,14 +6,6 @@ public class ScoreKeeperShould {
 
     @Test
     public void
-    should_display_game_score() {
-        ScoreKeeper scoreKeeper = new ScoreKeeper(new TeamScore(), new TeamScore());
-
-        assertThat(scoreKeeper.getScore()).isEqualTo("000:000");
-    }
-
-    @Test
-    public void
     should_display_correct_score_when_adding_points_to_a_team_A() {
         TeamScore teamScoreA = new TeamScore();
         TeamScore teamScoreB = new TeamScore();
@@ -38,5 +30,17 @@ public class ScoreKeeperShould {
         scoreKeeper.scoreTeamB3();
 
         assertThat(teamScoreB.getPoints()).isEqualTo(6);
+    }
+
+    @Test
+    public void
+    should_display_game_score() {
+        ScoreKeeper scoreKeeper = new ScoreKeeper(new TeamScore(), new TeamScore());
+        scoreKeeper.scoreTeamB1();
+        scoreKeeper.scoreTeamA3();
+        scoreKeeper.scoreTeamB2();
+        scoreKeeper.scoreTeamB3();
+
+        assertThat(scoreKeeper.getScore()).isEqualTo("003:006");
     }
 }
