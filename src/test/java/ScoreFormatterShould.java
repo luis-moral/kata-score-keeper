5bcde0397724651b72c988d89eba882cdf1711e4
Luis Moral Guerrero <luis.moral@gmail.com>
2018-08-13 18:17:44 +0200

    All test completed.

--- src/test/java/ScoreFormatterShould.java
+++ src/test/java/ScoreFormatterShould.java
@@ -1,13 +1,37 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@RunWith(Parameterized.class)
 public class ScoreFormatterShould {
+
+    private int pointsA;
+    private int pointsB;
+    private String expected;
+
+    @Parameterized.Parameters
+    public static Collection parameters() {
+        return Arrays.asList(new Object[][] {
+                { 0, 0, "000:000" },
+                { 1, 1, "001:001" },
+                { 9, 6, "009:006" }
+        });
+    }
+
+    public ScoreFormatterShould(int pointsA, int pointsB, String expected) {
+        this.pointsA = pointsA;
+        this.pointsB = pointsB;
+        this.expected = expected;
+    }
 
     @Test
     public void
     format_the_game_score() {
-
-        assertThat(ScoreFormatter.format(0,0)).isEqualTo("000:000");
+        assertThat(ScoreFormatter.format(pointsA, pointsB)).isEqualTo(expected);
     }
 }
